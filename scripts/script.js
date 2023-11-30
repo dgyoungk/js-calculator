6a797d32e1da2521382174c00f715428b5a6db4d
Daniel <150309766+dgyoungk@users.noreply.github.com>
2023-11-30 03:39:11 -0800

    Add function to create the calculator buttons

--- scripts/script.js
+++ scripts/script.js
@@ -55,3 +55,33 @@ for (let i = 0; i < 5; i ++) {
             break;
     }
 }
+
+let buttonText = [1, 2, 3, "+", 4, 5, 6, '-', 7, 8, 9, '*', 0, '.', '=', '/'];
+
+// DOM manipulations
+function createButtons() {
+    const btnContainer = document.querySelector('.btns');
+    for (let i = 0; i < 16; i++) {
+        const btn = document.createElement('button');
+        btn.style.width = '125px';
+        btn.style.height = '100px';
+        btn.style.border = "1px solid black";
+        btn.style.fontSize = '32px';
+        btn.innerText = buttonText[i];
+        /*
+            TODO:
+            figure out the condition to set the class for number buttons and operator buttons 
+        */
+        // if () {
+        //     btn.classList.toggle('num-btn');
+        // } else {
+        //     btn.classList.toggle('op-btn');
+        // }
+        btnContainer.appendChild(btn);
+    }
+}
+
+
+
+
+window.addEventListener('load', createButtons);
